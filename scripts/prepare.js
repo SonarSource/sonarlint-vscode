7cd3c096b70af263035b7d87a9f43260e62844bb
Julien HENRY <julien.henry@sonarsource.com>
2017-06-26 12:48:40 +0200

    SLVSCODE-15 Show rule description inside VSCode

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -11,7 +11,7 @@ if (!fs.existsSync('analyzers')){
     fs.mkdirSync('analyzers');
 }
 
-downloadIfNeeded('https://repox.sonarsource.com/sonarsource-public-builds/org/sonarsource/sonarlint/core/sonarlint-language-server/2.15.0.780/sonarlint-language-server-2.15.0.780.jar', 'server/sonarlint-ls.jar');
+downloadIfNeeded('https://repox.sonarsource.com/sonarsource-public-builds/org/sonarsource/sonarlint/core/sonarlint-language-server/2.15.0.789/sonarlint-language-server-2.15.0.789.jar', 'server/sonarlint-ls.jar');
 downloadIfNeeded('http://repo1.maven.org/maven2/org/sonarsource/javascript/sonar-javascript-plugin/3.1.0.5111/sonar-javascript-plugin-3.1.0.5111.jar', 'analyzers/sonarjs.jar');
 downloadIfNeeded('http://repo1.maven.org/maven2/org/sonarsource/php/sonar-php-plugin/2.10.0.2087/sonar-php-plugin-2.10.0.2087.jar', 'analyzers/sonarphp.jar');
 downloadIfNeeded('http://repo1.maven.org/maven2/org/sonarsource/python/sonar-python-plugin/1.8.0.1496/sonar-python-plugin-1.8.0.1496.jar', 'analyzers/sonarpython.jar');
@@ -33,7 +33,7 @@ function downloadIfChecksumMismatch(expectedChecksum, url, dest) {
         fs.createReadStream(dest).pipe(crypto.createHash('sha1').setEncoding('hex')).on('finish', function () {
             let sha1 = this.read();
             if (expectedChecksum != sha1) {
-                console.info("Checksum mismatch!");
+                console.info("Checksum mismatch for " + dest + ". Will download it!");
                 request(url).pipe(fs.createWriteStream(dest));
             }
         })
