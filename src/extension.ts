f449cf303a1290eb386295a1d6d16383902bf590
Julien HENRY <julien.henry@sonarsource.com>
2017-06-26 12:48:40 +0200

    SLVSCODE-15 Show rule description inside VSCode

--- src/extension.ts
+++ src/extension.ts
@@ -13,6 +13,7 @@ import * as Net from "net";
 import * as ChildProcess from "child_process";
 import {LanguageClient, LanguageClientOptions, StreamInfo} from "vscode-languageclient";
 import * as open from "open";
+import * as http from "http";
 import * as requirements from './requirements';
 
 declare var v8debug;
@@ -120,11 +121,91 @@ export function activate(context: VSCode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 	context.subscriptions.push(onConfigurationChange());
 
-	let disposableCommand = VSCode.commands.registerCommand('SonarLint.OpenRuleDesc', (ruleKey, ruleServerPort) => {
-		open("http://localhost:" + ruleServerPort + "/?ruleKey=" + encodeURIComponent(ruleKey));
+	var ruleDescPanelContent = 'No description';
+
+	class TextDocumentContentProvider implements VSCode.TextDocumentContentProvider {
+
+		private _onDidChange = new VSCode.EventEmitter<VSCode.Uri>();
+
+		get onDidChange(): VSCode.Event<VSCode.Uri> {
+			return this._onDidChange.event;
+		}
+
+		public update(uri: VSCode.Uri) {
+			this._onDidChange.fire(uri);
+		}
+		
+		public provideTextDocumentContent(uri: VSCode.Uri): string {
+			return ruleDescPanelContent;
+		}
+
+	}
+
+	let provider = new TextDocumentContentProvider();
+	let registration = VSCode.workspace.registerTextDocumentContentProvider('sonarlint-rule', provider);
+	context.subscriptions.push(registration);
+
+	let showRuleUri = VSCode.Uri.parse('sonarlint-rule://show');
+
+	let openRuleCommand = VSCode.commands.registerCommand('SonarLint.OpenRuleDesc', (ruleKey:string, ruleName:string, htmlDesc:string, ruleType:string, ruleSeverity:string) => {
+		ruleDescPanelContent = computeRuleDescPanelContent(context, ruleKey, ruleName, htmlDesc, ruleType, ruleSeverity);
+		VSCode.commands.executeCommand('vscode.previewHtml', showRuleUri, VSCode.ViewColumn.Two, 'SonarLint Rule Description')
+			.then((success) => {
+				provider.update(showRuleUri);
+			}, (reason) => {
+				VSCode.window.showErrorMessage(reason);
+			});
 	});
 
-	context.subscriptions.push(disposableCommand);
+	context.subscriptions.push(openRuleCommand);
+}
+
+function computeRuleDescPanelContent(context: VSCode.ExtensionContext, ruleKey:string, ruleName:string, htmlDesc:string, ruleType:string, ruleSeverity:string) {
+	let severityImg = Path.resolve(context.extensionPath, "images", "severity", ruleSeverity.toLowerCase() + ".png");
+	let typeImg = Path.resolve(context.extensionPath, "images", "type", ruleType.toLowerCase() + ".png");
+	return `<!doctype html><html>
+		<head>
+		<style type="text/css">
+			body { 
+				font-family: Helvetica Neue,Segoe UI,Helvetica,Arial,sans-serif; 
+				font-size: 13px; line-height: 1.23076923; 
+			}
+			
+			h1 { font-size: 14px;font-weight: 500; }
+			h2 { line-height: 24px;}
+			a { border-bottom: 1px solid rgba(230, 230, 230, .1); color: #236a97; cursor: pointer; outline: none; text-decoration: none; transition: all .2s ease;}
+			
+			.rule-desc { line-height: 1.5;}
+			.rule-desc { line-height: 1.5;}
+			.rule-desc h2 { font-size: 16px; font-weight: 400;}
+			.rule-desc code { padding: .2em .45em; margin: 0; border-radius: 3px; white-space: nowrap;}
+			.rule-desc pre { padding: 10px; border-top: 1px solid rgba(230, 230, 230, .1); border-bottom: 1px solid rgba(230, 230, 230, .1); line-height: 18px; overflow: auto;}
+			.rule-desc code, .rule-desc pre { font-family: Consolas,Liberation Mono,Menlo,Courier,monospace; font-size: 12px;}
+			.rule-desc ul { padding-left: 40px; list-style: disc;}
+		</style>
+		</head>
+		<body><h1><big>${ruleName}</big> (${ruleKey})</h1>
+		<div>
+		<img style="padding-bottom: 1px;vertical-align: middle" width="16" height="16" alt="${ruleType}" src="data:image/gif;base64,${base64_encode(typeImg)}">&nbsp;
+		${clean(ruleType)}&nbsp;
+		<img style="padding-bottom: 1px;vertical-align: middle" width="16" height="16" alt="${ruleSeverity}" src="data:image/gif;base64,${base64_encode(severityImg)}">&nbsp;
+		${clean(ruleSeverity)}
+		</div>
+		<div class=\"rule-desc\">${htmlDesc}</div>
+		</body></html>`;
+}
+
+function clean(str:string) {
+	return capitalizeName(str.toLowerCase().split('_').join(' '));
+}
+
+function capitalizeName(name:string) {
+  return name.replace(/\b(\w)/g, s => s.toUpperCase());
+}
+
+function base64_encode(file) {
+    var bitmap = FS.readFileSync(file);
+    return new Buffer(bitmap).toString('base64');
 }
 
 function logNotification(message:string, ...items: string[]) {
