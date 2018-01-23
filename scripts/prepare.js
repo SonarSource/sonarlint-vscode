8367f174ba9ecc33b68b089cf8c8ded77efcfbd7
Julien HENRY <julien.henry@sonarsource.com>
2018-01-23 17:17:16 +0100

    Apply prettier

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -1,42 +1,56 @@
-'use strict';
-const fs = require('fs');
-const crypto = require('crypto');
-const request = require('request');
+"use strict";
+const fs = require("fs");
+const crypto = require("crypto");
+const request = require("request");
 
-if (!fs.existsSync('server')){
-    fs.mkdirSync('server');
+if (!fs.existsSync("server")) {
+  fs.mkdirSync("server");
 }
 
-if (!fs.existsSync('analyzers')){
-    fs.mkdirSync('analyzers');
+if (!fs.existsSync("analyzers")) {
+  fs.mkdirSync("analyzers");
 }
 
-downloadIfNeeded('https://repox.sonarsource.com/sonarsource-public-releases/org/sonarsource/sonarlint/core/sonarlint-language-server/2.15.0.797/sonarlint-language-server-2.15.0.797.jar', 'server/sonarlint-ls.jar');
-downloadIfNeeded('http://repo1.maven.org/maven2/org/sonarsource/javascript/sonar-javascript-plugin/3.1.0.5111/sonar-javascript-plugin-3.1.0.5111.jar', 'analyzers/sonarjs.jar');
-downloadIfNeeded('http://repo1.maven.org/maven2/org/sonarsource/php/sonar-php-plugin/2.10.0.2087/sonar-php-plugin-2.10.0.2087.jar', 'analyzers/sonarphp.jar');
-downloadIfNeeded('http://repo1.maven.org/maven2/org/sonarsource/python/sonar-python-plugin/1.8.0.1496/sonar-python-plugin-1.8.0.1496.jar', 'analyzers/sonarpython.jar');
+downloadIfNeeded(
+  "https://repox.sonarsource.com/sonarsource-public-releases/org/sonarsource/sonarlint/core/sonarlint-language-server/2.15.0.797/sonarlint-language-server-2.15.0.797.jar",
+  "server/sonarlint-ls.jar"
+);
+downloadIfNeeded(
+  "http://repo1.maven.org/maven2/org/sonarsource/javascript/sonar-javascript-plugin/3.1.0.5111/sonar-javascript-plugin-3.1.0.5111.jar",
+  "analyzers/sonarjs.jar"
+);
+downloadIfNeeded(
+  "http://repo1.maven.org/maven2/org/sonarsource/php/sonar-php-plugin/2.10.0.2087/sonar-php-plugin-2.10.0.2087.jar",
+  "analyzers/sonarphp.jar"
+);
+downloadIfNeeded(
+  "http://repo1.maven.org/maven2/org/sonarsource/python/sonar-python-plugin/1.8.0.1496/sonar-python-plugin-1.8.0.1496.jar",
+  "analyzers/sonarpython.jar"
+);
 
 function downloadIfNeeded(url, dest) {
-    request(url + '.sha1', function (error, response, body) {
-        if (error) {
-            console.error('error:', error);
-        } else {
-            downloadIfChecksumMismatch(body, url, dest);
-        }
-    });
+  request(url + ".sha1", function(error, response, body) {
+    if (error) {
+      console.error("error:", error);
+    } else {
+      downloadIfChecksumMismatch(body, url, dest);
+    }
+  });
 }
 
 function downloadIfChecksumMismatch(expectedChecksum, url, dest) {
-    if (!fs.existsSync(dest)) {
-        request(url).pipe(fs.createWriteStream(dest));
-    } else {
-        fs.createReadStream(dest).pipe(crypto.createHash('sha1').setEncoding('hex')).on('finish', function () {
-            let sha1 = this.read();
-            if (expectedChecksum != sha1) {
-                console.info("Checksum mismatch for " + dest + ". Will download it!");
-                request(url).pipe(fs.createWriteStream(dest));
-            }
-        })
-    }
+  if (!fs.existsSync(dest)) {
+    request(url).pipe(fs.createWriteStream(dest));
+  } else {
+    fs
+      .createReadStream(dest)
+      .pipe(crypto.createHash("sha1").setEncoding("hex"))
+      .on("finish", function() {
+        let sha1 = this.read();
+        if (expectedChecksum != sha1) {
+          console.info("Checksum mismatch for " + dest + ". Will download it!");
+          request(url).pipe(fs.createWriteStream(dest));
+        }
+      });
+  }
 }
-
--- src/requirements.ts
+++ src/requirements.ts
@@ -1,102 +1,114 @@
 /* --------------------------------------------------------------------------------------------
  * SonarLint for VisualStudio Code
- * Copyright (C) 2017 SonarSource SA
+ * Copyright (C) 2017-2018 SonarSource SA
  * sonarlint@sonarsource.com
  * Licensed under the LGPLv3 License. See LICENSE.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
-'use strict';
+"use strict";
 
-import { workspace, Uri } from 'vscode';
-import * as cp from 'child_process';
-import * as path from 'path';
+import { workspace, Uri } from "vscode";
+import * as cp from "child_process";
+import * as path from "path";
 
-const pathExists = require('path-exists');
-const expandHomeDir = require('expand-home-dir');
-const findJavaHome = require('find-java-home');
-const isWindows = process.platform.indexOf('win') === 0;
-const JAVA_FILENAME = 'java' + (isWindows?'.exe':'');
+const pathExists = require("path-exists");
+const expandHomeDir = require("expand-home-dir");
+const findJavaHome = require("find-java-home");
+const isWindows = process.platform.indexOf("win") === 0;
+const JAVA_FILENAME = "java" + (isWindows ? ".exe" : "");
 
 interface RequirementsData {
-    java_home: string;
-    java_version: number;
+  java_home: string;
+  java_version: number;
 }
 
 interface ErrorData {
-    message: string;
-    label: string;
-    openUrl: Uri;
-    replaceClose: boolean;
+  message: string;
+  label: string;
+  openUrl: Uri;
+  replaceClose: boolean;
 }
 
 export async function resolveRequirements(): Promise<RequirementsData> {
-    let java_home = await checkJavaRuntime();
-    let javaVersion = await checkJavaVersion(java_home);
-    return Promise.resolve({ 'java_home': java_home, 'java_version': javaVersion});
+  let java_home = await checkJavaRuntime();
+  let javaVersion = await checkJavaVersion(java_home);
+  return Promise.resolve({ java_home: java_home, java_version: javaVersion });
 }
 
 function checkJavaRuntime(): Promise<any> {
-    return new Promise((resolve, reject) => {
-        let source : string;
-        let javaHome : string = readJavaConfig();
-        if (javaHome) {
-            source = 'The \'sonarlint.ls.javaHome\' variable defined in VS Code settings';
-        } else {
-            javaHome = process.env['JDK_HOME'];
-            if (javaHome) {
-                source = 'The JDK_HOME environment variable';
-            } else {
-                javaHome = process.env['JAVA_HOME'];
-                source = 'The JAVA_HOME environment variable';
-            }
-        }
-        if (javaHome) {
-            javaHome = expandHomeDir(javaHome);
-            if (!pathExists.sync(javaHome)) {
-                openJREDownload(reject, source + ' points to a missing folder');
-            }
-            if (!pathExists.sync(path.resolve(javaHome, 'bin', JAVA_FILENAME))){
-                openJREDownload(reject, source + ' does not point to a JRE.');
-            }
-            return resolve(javaHome);
-        }
-        //No settings, let's try to detect as last resort.
-        findJavaHome(function (err, home) {
-                if (err) {
-                    openJREDownload(reject, 'Java runtime could not be located. Install it and set its location using \'sonarlint.ls.javaHome\' variable in VS Code settings.');
-                }
-                else {
-                    resolve(home);
-                }
-            });
+  return new Promise((resolve, reject) => {
+    let source: string;
+    let javaHome: string = readJavaConfig();
+    if (javaHome) {
+      source =
+        "The 'sonarlint.ls.javaHome' variable defined in VS Code settings";
+    } else {
+      javaHome = process.env["JDK_HOME"];
+      if (javaHome) {
+        source = "The JDK_HOME environment variable";
+      } else {
+        javaHome = process.env["JAVA_HOME"];
+        source = "The JAVA_HOME environment variable";
+      }
+    }
+    if (javaHome) {
+      javaHome = expandHomeDir(javaHome);
+      if (!pathExists.sync(javaHome)) {
+        openJREDownload(reject, source + " points to a missing folder");
+      }
+      if (!pathExists.sync(path.resolve(javaHome, "bin", JAVA_FILENAME))) {
+        openJREDownload(reject, source + " does not point to a JRE.");
+      }
+      return resolve(javaHome);
+    }
+    //No settings, let's try to detect as last resort.
+    findJavaHome(function(err, home) {
+      if (err) {
+        openJREDownload(
+          reject,
+          "Java runtime could not be located. Install it and set its location using 'sonarlint.ls.javaHome' variable in VS Code settings."
+        );
+      } else {
+        resolve(home);
+      }
     });
+  });
 }
 
-function readJavaConfig() : string {
-    const config = workspace.getConfiguration();
-    return config.get<string>('sonarlint.ls.javaHome',null);
+function readJavaConfig(): string {
+  const config = workspace.getConfiguration();
+  return config.get<string>("sonarlint.ls.javaHome", null);
 }
 
 function checkJavaVersion(java_home: string): Promise<any> {
-    return new Promise((resolve, reject) => {
-        cp.execFile(java_home + '/bin/java', ['-version'], {}, (error, stdout, stderr) => {
-            if (stderr.indexOf('version "9') > -1){
-                resolve(9);
-            } if (stderr.indexOf('1.8') < 0){
-                openJREDownload(reject, 'Java 8 is required to run. Please download and install a JRE 8.');
-            }
-            else{
-                resolve(8);
-            }
-        });
-    });
+  return new Promise((resolve, reject) => {
+    cp.execFile(
+      java_home + "/bin/java",
+      ["-version"],
+      {},
+      (error, stdout, stderr) => {
+        if (stderr.indexOf('version "9') > -1) {
+          resolve(9);
+        }
+        if (stderr.indexOf("1.8") < 0) {
+          openJREDownload(
+            reject,
+            "Java 8 is required to run. Please download and install a JRE 8."
+          );
+        } else {
+          resolve(8);
+        }
+      }
+    );
+  });
 }
 
 function openJREDownload(reject, cause) {
-    let jreUrl = 'http://www.oracle.com/technetwork/java/javase/downloads/index.html';
-    reject({
-        message: cause,
-        label: 'Get Java Runtime Environment',
-        openUrl: Uri.parse(jreUrl),
-        replaceClose: false
-    });
+  let jreUrl =
+    "http://www.oracle.com/technetwork/java/javase/downloads/index.html";
+  reject({
+    message: cause,
+    label: "Get Java Runtime Environment",
+    openUrl: Uri.parse(jreUrl),
+    replaceClose: false
+  });
 }
