4d96331d8c625419055e046b732a675f5223b80f
Julien HENRY <julien.henry@sonarsource.com>
2018-01-25 17:28:11 +0100

    SLVSCODE-17 Add support for Vue.js

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -12,7 +12,7 @@ if (!fs.existsSync("analyzers")) {
 }
 
 downloadIfNeeded(
-  "https://repox.sonarsource.com/sonarsource/org/sonarsource/sonarlint/core/sonarlint-language-server/3.1.0.1343/sonarlint-language-server-3.1.0.1343.jar",
+  "https://repox.sonarsource.com/sonarsource/org/sonarsource/sonarlint/core/sonarlint-language-server/3.1.0.1367/sonarlint-language-server-3.1.0.1367.jar",
   "server/sonarlint-ls.jar"
 );
 downloadIfNeeded(
