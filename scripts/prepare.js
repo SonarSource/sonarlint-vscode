cebdb8c13ca7a339f4de1236e5600320c459b1b9
Janos Gyerik <janos.gyerik@sonarsource.com>
2018-04-30 11:05:48 +0200

    Update language server version DO NOT MERGE update before release

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -12,7 +12,7 @@ if (!fs.existsSync("analyzers")) {
 }
 
 downloadIfNeeded(
-  "https://repox.sonarsource.com/sonarsource/org/sonarsource/sonarlint/core/sonarlint-language-server/3.1.0.1376/sonarlint-language-server-3.1.0.1376.jar",
+  "https://repox.sonarsource.com/sonarsource/org/sonarsource/sonarlint/core/sonarlint-language-server/3.5.0.1548/sonarlint-language-server-3.5.0.1548.jar",
   "server/sonarlint-ls.jar"
 );
 downloadIfNeeded(
