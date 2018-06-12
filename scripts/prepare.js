d648b850e1e78612039d13ab7c59b1ba9fa2dc99
Janos Gyerik <janos.gyerik@sonarsource.com>
2018-06-12 14:15:30 +0200

    SLVSCODE-38 SLVSCODE-40 Use language server version with bugfixes
    - Fix uri-path transformations
    - Fix bogus error pop-up

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const crypto = require("crypto");
 const request = require("request");
 
-const languageServerVersion = '3.6.0.1693';
+const languageServerVersion = '3.7.1.1756';
 const sonarJsVersion = '4.1.0.6085';
 const sonarPhpVersion = '2.13.0.3107';
 const sonarPythonVersion = '1.10.0.2131';
@@ -18,7 +18,8 @@ if (!fs.existsSync("analyzers")) {
 }
 
 downloadIfNeeded(
-  `https://repo1.maven.org/maven2/org/sonarsource/sonarlint/core/sonarlint-language-server/${languageServerVersion}/sonarlint-language-server-${languageServerVersion}.jar`,
+  //`https://repo1.maven.org/maven2/org/sonarsource/sonarlint/core/sonarlint-language-server/${languageServerVersion}/sonarlint-language-server-${languageServerVersion}.jar`,
+  `https://repox.sonarsource.com/sonarsource/org/sonarsource/sonarlint/core/sonarlint-language-server/${languageServerVersion}/sonarlint-language-server-${languageServerVersion}.jar`,
   "server/sonarlint-ls.jar"
 );
 downloadIfNeeded(
