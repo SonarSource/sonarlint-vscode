ed352d9905af56a9510d52cacbcbc6b4f2fcad4a
Julien HENRY <julien.henry@sonarsource.com>
2018-08-07 13:22:43 +0200

    SLVSCODE-27 Support secondary locations

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -3,11 +3,11 @@ const fs = require("fs");
 const crypto = require("crypto");
 const request = require("request");
 
-const languageServerVersion = '3.7.1.1756';
-const sonarJsVersion = '4.1.0.6085';
-const sonarPhpVersion = '2.13.0.3107';
-const sonarPythonVersion = '1.10.0.2131';
-const sonarTsVersion = '1.7.0.2893';
+const languageServerVersion = "3.7.1.1756";
+const sonarJsVersion = "4.1.0.6085";
+const sonarPhpVersion = "2.13.0.3107";
+const sonarPythonVersion = "1.10.0.2131";
+const sonarTsVersion = "1.7.0.2893";
 
 if (!fs.existsSync("server")) {
   fs.mkdirSync("server");
@@ -18,7 +18,7 @@ if (!fs.existsSync("analyzers")) {
 }
 
 downloadIfNeeded(
-  `https://repo1.maven.org/maven2/org/sonarsource/sonarlint/core/sonarlint-language-server/${languageServerVersion}/sonarlint-language-server-${languageServerVersion}.jar`,
+  `https://repox.sonarsource.com/sonarsource/org/sonarsource/sonarlint/core/sonarlint-language-server/3.9.0.1876/sonarlint-language-server-3.9.0.1876.jar`,
   "server/sonarlint-ls.jar"
 );
 downloadIfNeeded(
@@ -39,21 +39,26 @@ downloadIfNeeded(
 );
 
 function downloadIfNeeded(url, dest) {
-  request(url + ".sha1", function(error, response, body) {
-    if (error) {
-      console.error("error:", error);
-    } else {
-      downloadIfChecksumMismatch(body, url, dest);
-    }
-  });
+  if (url.startsWith("file:")) {
+    fs.createReadStream(url.substring("file:".length)).pipe(
+      fs.createWriteStream(dest)
+    );
+  } else {
+    request(url + ".sha1", function(error, response, body) {
+      if (error) {
+        console.error("error:", error);
+      } else {
+        downloadIfChecksumMismatch(body, url, dest);
+      }
+    });
+  }
 }
 
 function downloadIfChecksumMismatch(expectedChecksum, url, dest) {
   if (!fs.existsSync(dest)) {
     request(url).pipe(fs.createWriteStream(dest));
   } else {
-    fs
-      .createReadStream(dest)
+    fs.createReadStream(dest)
       .pipe(crypto.createHash("sha1").setEncoding("hex"))
       .on("finish", function() {
         let sha1 = this.read();
