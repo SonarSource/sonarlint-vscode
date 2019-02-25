08ca35ffb5e06695772df9b7e1237ba5c3f7e0ef
Julien HENRY <julien.henry@sonarsource.com>
2019-02-25 11:51:13 +0100

    Update license header

--- src/requirements.ts
+++ src/requirements.ts
@@ -1,6 +1,6 @@
 /* --------------------------------------------------------------------------------------------
  * SonarLint for VisualStudio Code
- * Copyright (C) 2017-2018 SonarSource SA
+ * Copyright (C) 2017-2019 SonarSource SA
  * sonarlint@sonarsource.com
  * Licensed under the LGPLv3 License. See LICENSE.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
