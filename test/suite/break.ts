f69f14f4190c945c2b2752d9917da711ec55778e
Jean-Baptiste Lievremont <jeanbaptiste.lievremont@sonarsource.com>
2019-10-24 15:59:42 +0200

    Intentionally break unit tests

---
+++ test/suite/break.ts
@@ -0,0 +1,14 @@
+/* --------------------------------------------------------------------------------------------
+ * SonarLint for VisualStudio Code
+ * Copyright (C) 2017-2019 SonarSource SA
+ * sonarlint@sonarsource.com
+ * Licensed under the LGPLv3 License. See LICENSE.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import { expect } from 'chai';
+
+describe('Break CI', () => {
+  it('Should fail CI on failed test assertion', () => {
+    expect(false).to.be.true;
+  });
+});
