52f660878b350029ed8cfde7bf071c84bbc3b4a9
Julien HENRY <julien.henry@sonarsource.com>
2019-12-16 18:13:37 +0100

    SLVSCODE-106 Update the language server to 1.0.1

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -14,7 +14,7 @@ const jarDependencies = [
   {
     groupId: 'org/sonarsource/sonarlint/ls',
     artifactId: 'sonarlint-language-server',
-    version: '1.0.0.12614',
+    version: '1.0.1.13655',
     output: 'server/sonarlint-ls.jar'
   },
   {
