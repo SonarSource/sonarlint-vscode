a68ce82153506fddcf1f39b80746dc6a7751bc85
Julien HENRY <julien.henry@sonarsource.com>
2020-02-25 16:31:30 +0100

    Add IT

---
+++ its/samples/sample-java-maven-multi-module/modules-level2/src/main/java/edu/marcelo/App.java
@@ -0,0 +1,13 @@
+package edu.marcelo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
--- src/client.ts
+++ src/client.ts
@@ -5,21 +5,15 @@
  * Licensed under the LGPLv3 License. See LICENSE.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 'use strict';
-import {
-  LanguageClient
-} from 'vscode-languageclient';
-import {
-  RulesResponse
-} from './rules';
+import { LanguageClient } from 'vscode-languageclient';
+import { RulesResponse } from './rules';
 
 export class SonarLintExtendedLanguageClient extends LanguageClient {
   listAllRules(): Thenable<RulesResponse> {
     return this.sendRequest('sonarlint/listAllRules');
   }
 
-  didClasspathUpdate(projectUri: string): void {
-    this.sendNotification('sonarlint/didClasspathUpdate', projectUri);
+  didClasspathUpdate(projectRoot: string): void {
+    this.sendNotification('sonarlint/didClasspathUpdate', projectRoot);
   }
-
-
 }
