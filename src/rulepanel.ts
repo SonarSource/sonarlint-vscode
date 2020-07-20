8a1a42f6cc4b8a1e15530ad65d6ae41a5656b713
Jean-Baptiste Lièvremont <jeanbaptiste.lievremont@sonarsource.com>
2020-07-20 16:51:08 +0200

    SLVSCODE-137 Show parameters with value from config in rule description view (#103)
    * SLVSCODE-137 Show parameters in rule description view
    
    * SLVSCODE-137 Apply PR feedback

--- src/rulepanel.ts
+++ src/rulepanel.ts
@@ -12,12 +12,13 @@ import * as VSCode from 'vscode';
 import { ShowRuleDescriptionParams } from './protocol';
 
 export function computeRuleDescPanelContent(context: VSCode.ExtensionContext, rule: ShowRuleDescriptionParams) {
-  const severityImg = base64_encode(
-    Path.resolve(context.extensionPath, 'images', 'severity', rule.severity.toLowerCase() + '.png')
+  const severityImg = base64encode(
+    Path.resolve(context.extensionPath, 'images', 'severity', `${rule.severity.toLowerCase()}.png`)
   );
-  const typeImg = base64_encode(
-    Path.resolve(context.extensionPath, 'images', 'type', rule.type.toLowerCase() + '.png')
+  const typeImg = base64encode(
+    Path.resolve(context.extensionPath, 'images', 'type', `${rule.type.toLowerCase()}.png`)
   );
+  const ruleParamsHtml = renderRuleParams(rule);
 
   return `<!doctype html><html>
 		<head>
@@ -25,23 +26,29 @@ export function computeRuleDescPanelContent(context: VSCode.ExtensionContext, ru
 		<meta http-equiv="Encoding" content="utf-8" />
 		<meta http-equiv="Content-Security-Policy" value="default-src 'none'"/>
 		<style type="text/css">
-			body { 
-				font-family: Helvetica Neue,Segoe UI,Helvetica,Arial,sans-serif; 
-				font-size: 13px; line-height: 1.23076923; 
+			body {
+				font-family: 'Helvetica Neue','Segoe UI',Helvetica,Arial,sans-serif;
+				font-size: 13px; line-height: 1.23076923;
 			}
-			
+
 			h1 { font-size: 14px;font-weight: 500; }
 			h2 { line-height: 24px;}
 			a { border-bottom: 1px solid rgba(230, 230, 230, .1); color: #236a97; cursor: pointer; outline: none; text-decoration: none; transition: all .2s ease;}
-			
-			.rule-desc { line-height: 1.5;}
+
 			.rule-desc { line-height: 1.5;}
 			.rule-desc h2 { font-size: 16px; font-weight: 400;}
 			.rule-desc code { padding: .2em .45em; margin: 0; border-radius: 3px; white-space: nowrap;}
 			.rule-desc pre { padding: 10px; border-top: 1px solid rgba(230, 230, 230, .1); border-bottom: 1px solid rgba(230, 230, 230, .1); line-height: 18px; overflow: auto;}
 			.rule-desc code, .rule-desc pre { font-family: Consolas,Liberation Mono,Menlo,Courier,monospace; font-size: 12px;}
 			.rule-desc ul { padding-left: 40px; list-style: disc;}
-		</style>
+			.rule-params { border: none; border-collapse: collapse; padding: 1em; }
+			.rule-params caption { font-size: 16px; font-weight: 400; text-align: left; margin-bottom: 16px}
+			.rule-params thead td { vertical-align: top; padding-left: 0; padding-bottom: 1em; font-style: italic }
+			.rule-params tbody th { vertical-align: top; text-align: right; font-weight: inherit; font-family: monospace }
+			.rule-params tbody td { vertical-align: top; padding-left: 1em; padding-bottom: 1em; }
+			.rule-params p { margin: 0 }
+			.rule-params small { display: block; margin-top: 2px }
+			</style>
 		</head>
 		<body><h1><big>${escapeHtml(rule.name)}</big> (${rule.key})</h1>
 		<div>
@@ -54,7 +61,8 @@ export function computeRuleDescPanelContent(context: VSCode.ExtensionContext, ru
     }" src="data:image/gif;base64,${severityImg}">&nbsp;
 		${clean(rule.severity)}
 		</div>
-		<div class=\"rule-desc\">${rule.htmlDescription}</div>
+    <div class="rule-desc">${rule.htmlDescription}</div>
+    ${ruleParamsHtml}
 		</body></html>`;
 }
 
@@ -88,6 +96,43 @@ function capitalizeName(name: string) {
   return name.replace(/\b(\w)/g, s => s.toUpperCase());
 }
 
-function base64_encode(file: string) {
+function base64encode(file: string) {
   return FS.readFileSync(file).toString('base64');
+}
+
+function renderRuleParams(rule: ShowRuleDescriptionParams) {
+  if (rule.parameters && rule.parameters.length > 0) {
+    const ruleParamsConfig = VSCode.workspace.getConfiguration(`sonarlint.rules.${rule.key}.parameters`);
+    return `<table class="rule-params">
+  <caption>Parameters</caption>
+  <thead>
+    <tr>
+      <td colspan="2">
+        Following parameter values can be set in the <em>SonarLint:Rules</em> user settings.
+        In connected mode, server side configuration overrides local settings.
+      </td>
+    </tr>
+  </thead>
+  <tbody>
+    ${rule.parameters.map(p => renderRuleParam(p, ruleParamsConfig)).join('\n')}
+  </tbody>
+</table>`;
+  } else {
+    return '';
+  }
+}
+
+function renderRuleParam(param, config) {
+  const { name, description, defaultValue } = param;
+  const descriptionP = description ? `<p>${description}</p>` : '';
+  const currentValue = config.has(name) ? `<small>Current value: <code>${config.get(name)}</code></small>` : '';
+  const defaultRendered = defaultValue ? `<small>(Default value: <code>${defaultValue}</code>)</small>` : '';
+  return `<tr>
+  <th>${name}</th>
+  <td>
+    ${descriptionP}
+    ${currentValue}
+    ${defaultRendered}
+  </td>
+</tr>`;
 }
