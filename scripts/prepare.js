1ff9906a77c3f60e5b6898cf383bd3c7b2e4d055
knize <kirill.knize@sonarsource.com>
2021-08-03 18:12:17 +0200

    SLVSCODE-187 make private analyzers downloading optional

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -19,6 +19,8 @@ const auth = {
   pass: ARTIFACTORY_PRIVATE_READER_PASSWORD,
   sendImmediate: true
 };
+const credentialsDefined = ARTIFACTORY_PRIVATE_READER_USERNAME !== undefined
+    && ARTIFACTORY_PRIVATE_READER_PASSWORD !== undefined;
 
 const repoxRoot = 'https://repox.jfrog.io/repox/sonarsource';
 const jarDependencies = require('./dependencies.json');
@@ -34,7 +36,10 @@ if (!fs.existsSync('analyzers')) {
 }
 
 jarDependencies.map(dep => {
-   downloadIfNeeded(artifactUrl(dep), dep.output);
+  if (dep.requiresCredentials && !credentialsDefined) {
+    return;
+  }
+  downloadIfNeeded(artifactUrl(dep), dep.output);
 });
 
 function artifactUrl(dep) {
@@ -46,7 +51,7 @@ function downloadIfNeeded(url, dest) {
   if (url.startsWith('file:')) {
     fs.createReadStream(url.substring('file:'.length)).pipe(fs.createWriteStream(dest));
   } else {
-    request(url + '.sha1', { auth }, (error, response, body) => {
+    const callback = (error, response, body) => {
       if (error) {
         throw error;
       } else if (response.statusCode !== HTTP_OK) {
@@ -54,14 +59,22 @@ function downloadIfNeeded(url, dest) {
       } else {
         downloadIfChecksumMismatch(body, url, dest);
       }
-    });
+    };
+    if (credentialsDefined) {
+      request(url + '.sha1', {auth}, callback);
+    } else {
+      request(url + '.sha1', callback);
+    }
   }
 }
 
 function downloadIfChecksumMismatch(expectedChecksum, url, dest) {
   if (!fs.existsSync(dest)) {
-    request(url, { auth }).pipe(fs.createWriteStream(dest));
-
+    if (credentialsDefined) {
+      request(url, { auth }).pipe(fs.createWriteStream(dest));
+    } else {
+      request(url).pipe(fs.createWriteStream(dest));
+    }
   } else {
     fs.createReadStream(dest)
       .pipe(crypto.createHash('sha1').setEncoding('hex'))
