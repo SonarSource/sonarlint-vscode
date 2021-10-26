65dd0ef7dd6bc3d687d05ac59ac77d9f77fa51a8
Jean-Baptiste Lievremont <jeanbaptiste.lievremont@sonarsource.com>
2021-10-26 10:11:41 +0200

    Update mocha in its

--- its/src/test/javaSuite/index.ts
+++ its/src/test/javaSuite/index.ts
@@ -4,9 +4,9 @@
  * sonarlint@sonarsource.com
  * Licensed under the LGPLv3 License. See LICENSE.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
-import * as path from 'path';
-import * as Mocha from 'mocha';
 import * as glob from 'glob';
+import * as Mocha from 'mocha';
+import * as path from 'path';
 
 export function run(testsRoot: string, cb: (error: any, failures?: number) => void): void {
   // Create the mocha test
@@ -18,9 +18,9 @@ export function run(testsRoot: string, cb: (error: any, failures?: number) => vo
       xunitReporterOptions: {
         output: path.resolve(__dirname, '..', '..', 'java-tests.xml')
       }
-    }
+    },
+    color: true
   });
-  mocha.useColors(true);
 
   glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
     if (err) {
