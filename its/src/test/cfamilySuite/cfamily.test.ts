c20f3654d83d2a6863680b6d995f77be09755140
knize <kirill.knize@sonarsource.com>
2022-03-21 17:47:07 +0100

    its working

--- its/src/test/cfamilySuite/cfamily.test.ts
+++ its/src/test/cfamilySuite/cfamily.test.ts
@@ -6,16 +6,14 @@
  * ------------------------------------------------------------------------------------------ */
 import * as assert from 'assert';
 import * as path from 'path';
-import * as CompareVersions from 'compare-versions';
+import * as nutTree from '@nut-tree/nut-js';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
+import {Key} from "@nut-tree/nut-js";
 const fs = require("fs");
 
-import { waitForSonarLintDiagnostics } from '../common/util';
-
-const sampleFolderLocation = '../../../samples/';
 const sampleCfamilyFolderLocation = '../../../samples/sample-cfamily/';
 
 suite('CFamily Test Suite', () => {
@@ -25,32 +23,49 @@ suite('CFamily Test Suite', () => {
     assert.ok(vscode.extensions.getExtension('sonarsource.sonarlint-vscode'));
   });
 
-  test('should report issue on cpp file',  async () => {
-    vscode.workspace.getConfiguration().update('sonarlint.pathToCompileCommands', "");
-
-
+  test('should report issue on cpp file', async () => {
+    vscode.workspace.getConfiguration().update('sonarlint.pathToCompileCommands', undefined, vscode.ConfigurationTarget.WorkspaceFolder);
     const ext = vscode.extensions.getExtension('sonarsource.sonarlint-vscode')!;
     await ext.activate();
     const fileUri = vscode.Uri.file(path.join(__dirname, sampleCfamilyFolderLocation, 'main.cpp'));
 
-
-    const projectPath = vscode.Uri.file(path.join(__dirname, sampleCfamilyFolderLocation));
+    const projectUri = vscode.Uri.file(path.join(__dirname, sampleCfamilyFolderLocation));
 
     const document = await vscode.workspace.openTextDocument(fileUri);
     await vscode.window.showTextDocument(document);
     await vscode.commands.executeCommand('SonarLint.ConfigureCompilationDatabase');
-    const emptyPathToCompileCommands = vscode.workspace.getConfiguration().get('sonarlint.pathToCompileCommands');
-    console.debug("emptyPathToCompileCommands: " + JSON.stringify(emptyPathToCompileCommands));
+    const emptyPathToCompileCommands = vscode.workspace.getConfiguration('sonarlint', projectUri).get('pathToCompileCommands');
+    assert.equal(emptyPathToCompileCommands, '');
 
-    createCompilationDatabase(sampleCfamilyFolderLocation);
+    const firstCompileDbToCreate = vscode.Uri.file(path.join(__dirname, sampleCfamilyFolderLocation, 'compile_commands.json'));
+    createCompilationDatabase(firstCompileDbToCreate.path);
+    await vscode.commands.executeCommand('SonarLint.ConfigureCompilationDatabase');
+    let pathToCompileCommands = vscode.workspace.getConfiguration('sonarlint', projectUri).get('pathToCompileCommands');
+    assert.equal(pathToCompileCommands, firstCompileDbToCreate.path);
 
-
+    const secondFolderToCreate = vscode.Uri.file(path.join(__dirname, sampleCfamilyFolderLocation, "inner", 'compile_commands.json'));
+    createCompilationDatabase(secondFolderToCreate.path);
+    vscode.commands.executeCommand('SonarLint.ConfigureCompilationDatabase');
+    await sleep(2000);
+    await nutTree.keyboard.pressKey(Key.Down);
+    await nutTree.keyboard.releaseKey(Key.Down);
+    await sleep(2000);
+    await nutTree.keyboard.pressKey(Key.Enter);
+    await nutTree.keyboard.releaseKey(Key.Enter);
+    await sleep(2000);
+    pathToCompileCommands = vscode.workspace.getConfiguration('sonarlint', projectUri).get('pathToCompileCommands');
+    assert.equal(pathToCompileCommands, secondFolderToCreate.path);
     vscode.commands.executeCommand('workbench.action.closeActiveEditor');
-  }).timeout(10*1000);
+  }).timeout(10 * 1000);
 
 });
 
-function createCompilationDatabase(path:string) {
+function sleep(ms:number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+function createCompilationDatabase(file: string) {
   let compilationDbContent = "[\n" +
     "{\n" +
     "  \"directory\": \"/home/knize/CLionProjects/TT/cmake-build-debug\",\n" +
@@ -59,5 +74,5 @@ function createCompilationDatabase(path:string) {
     "}\n" +
     "]";
 
-  fs.writeFileSync(`${path}compile_commands.json`, compilationDbContent);
+  fs.writeFileSync(file, compilationDbContent);
 }
