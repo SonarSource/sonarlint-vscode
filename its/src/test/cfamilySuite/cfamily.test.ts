871ba5787c90f5d5563974cbc81db6c6d15635e8
knize <kirill.knize@sonarsource.com>
2022-03-22 13:14:37 +0100

    remove dependency

--- its/src/test/cfamilySuite/cfamily.test.ts
+++ its/src/test/cfamilySuite/cfamily.test.ts
@@ -6,7 +6,6 @@
  * ------------------------------------------------------------------------------------------ */
 import * as assert from 'assert';
 import * as path from 'path';
-import * as nutTree from '@nut-tree/nut-js';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
