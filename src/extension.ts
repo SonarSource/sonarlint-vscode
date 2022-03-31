d36b10987c52615920ae39fb4ff84fff2dbc3d03
Knize <62898885+kirill-knize-sonarsource@users.noreply.github.com>
2022-03-31 12:33:32 +0000

    SLVSCODE-246 Change notification message

--- src/extension.ts
+++ src/extension.ts
@@ -491,7 +491,7 @@ function installCustomRequestHandlers(context: VSCode.ExtensionContext) {
     }
     const doNotAskAgainAction = 'Do not ask again';
     const configureCompileCommandsAction = 'Configure compile commands';
-    const message = `SonarLint was unable to analyze the C/C++ file because there is no configured compilation 
+    const message = `SonarLint is unable to analyze C and C++ file(s) because there is no configured compilation 
     database.`;
     VSCode.window.showWarningMessage(message, doNotAskAgainAction, configureCompileCommandsAction).then(selection => {
       if (doNotAskAgainAction === selection) {
