1e36e81b66610e2f9a063a1cdeb544f44a5cae31
Jean-Baptiste Lievremont <jeanbaptiste.lievremont@sonarsource.com>
2022-04-27 15:47:35 +0200

    SLVSCODE-240 Add action to publish to VS marketplace

---
+++ .github/actions/vsce-publish/README.md
@@ -0,0 +1,8 @@
+vsce-publish
+============
+
+After the main release process has succeeded, publish the promoted artifact to 
+[the Visual Studio Marketplace](https://marketplace.visualstudio.com/items?itemName=SonarSource.sonarlint-vscode).
+
+Requires secret `VSCE_TOKEN` to contain a personal access token that has
+publisher rights to the [SonarSource publisher account](https://marketplace.visualstudio.com/publishers/SonarSource).
---
+++ .github/actions/vsce-publish/main.js
@@ -0,0 +1,40 @@
+/* --------------------------------------------------------------------------------------------
+ * SonarLint for VisualStudio Code
+ * Copyright (C) 2017-2022 SonarSource SA
+ * sonarlint@sonarsource.com
+ * Licensed under the LGPLv3 License. See LICENSE.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+/*
+ * Inspired from the open-vsx/publish-extensions script
+ * https://github.com/open-vsx/publish-extensions/blob/b120c07520489d1c9da11b9cc388e4b42607bc00/publish-extension.js
+ * Published under the terms of the Eclipse Public License v. 2.0
+ * http://www.eclipse.org/legal/epl-2.0
+ */
+
+'use strict';
+
+import * as vsce from 'vsce';
+
+const {
+  ARTIFACT_FILE,
+  VSCE_TOKEN
+} = process.env;
+
+(async () => {
+  /**
+   * @type vsce.IPublishVSIXOptions
+   */
+  const options = {
+    pat: VSCE_TOKEN
+  };
+  await vsce.publishVSIX(ARTIFACT_FILE, options);
+
+})()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
