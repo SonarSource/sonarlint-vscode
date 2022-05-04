60dc0bf8c59bd4eb33c7a7119d6058106b40ed2c
knize <kirill.knize@sonarsource.com>
2022-05-04 17:37:55 +0200

    log 4 prepare.js

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -71,6 +71,7 @@ function downloadIfNeeded(url, dest) {
 
 function downloadIfChecksumMismatch(expectedChecksum, url, dest) {
   if (!fs.existsSync(dest)) {
+    console.info(`File doesn't exists: '${dest}'. Will download it!`);
     sendRequest(url).pipe(fs.createWriteStream(dest));
   } else {
     fs.createReadStream(dest)
