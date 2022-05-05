b3e65936583443ddd939d3dbf6f8a14394d209c5
knize <kirill.knize@sonarsource.com>
2022-05-05 13:58:22 +0200

    wip

--- gulpfile.js
+++ gulpfile.js
@@ -253,7 +253,7 @@ function downloadJreAndInstallVsixForPlatform(platform) {
   };
 }
 
-const deployAllPlatformsSeries = () => {
+const deployAllPlatformsSeries = (done) => {
   const tasks = [];
   for (let i in platforms) {
     const platform = platforms[i];
@@ -269,6 +269,7 @@ const deployAllPlatformsSeries = () => {
   // };
   return gulp.series(...tasks, (seriesDone) => {
     seriesDone();
+    done();
   })();
 };
 
