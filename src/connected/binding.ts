33a99ae763c050cedf9fa1a4ed39035139286157
Sophio Japharidze <sophio.japharidze@sonarsource.com>
2022-09-16 16:45:30 +0200

    Remove extra space from quickpick instructions

--- src/connected/binding.ts
+++ src/connected/binding.ts
@@ -166,8 +166,7 @@ export class BindingService {
     if (remoteProjects) {
       const remoteProjectsQuickPick = VSCode.window.createQuickPick();
       remoteProjectsQuickPick.title = `Select ${serverType} Project to Bind with '${selectedFolderName}/'`;
-      remoteProjectsQuickPick.placeholder = `Select the remote project you want to bind with 
-      '${selectedFolderName}/' folder`;
+      remoteProjectsQuickPick.placeholder = `Select the remote project you want to bind with '${selectedFolderName}/' folder`;
       remoteProjectsQuickPick.items = [
         suggestedProjectsGroup,
         ...suggestedProjects,
