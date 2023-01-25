97014bbf0f562afc32d509709d0bc1539df040bd
Jean-Baptiste Lievremont <jeanbaptiste.lievremont@sonarsource.com>
2023-01-25 15:23:14 +0100

    Happy new year 2023

--- src/lsp/protocol.ts
+++ src/lsp/protocol.ts
@@ -1,6 +1,6 @@
 /* --------------------------------------------------------------------------------------------
  * SonarLint for VisualStudio Code
- * Copyright (C) 2017-2022 SonarSource SA
+ * Copyright (C) 2017-2023 SonarSource SA
  * sonarlint@sonarsource.com
  * Licensed under the LGPLv3 License. See LICENSE.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
