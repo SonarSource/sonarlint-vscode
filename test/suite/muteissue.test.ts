b5e9fb5cec7a3bb349d2150ea26e2a03e406b3e6
Vojtech Suchy <vojtech.suchy@sonarsource.com>
2023-06-07 07:15:28 +0200

    Address review comments

--- test/suite/muteissue.test.ts
+++ test/suite/muteissue.test.ts
@@ -3,7 +3,7 @@ import { MUTE_TRANSITION_STATES, translateQuickPickToIssueStatus } from '../../s
 
 suite('Mute Issue Test Suite', () => {
   test('getChildren should return all help and feedback view items', () => {
-    assert.equal(translateQuickPickToIssueStatus(MUTE_TRANSITION_STATES[0]), 'WONT_FIX')
-    assert.equal(translateQuickPickToIssueStatus(MUTE_TRANSITION_STATES[1]), 'FALSE_POSITIVE')
+    assert.equal(translateQuickPickToIssueStatus(MUTE_TRANSITION_STATES[0]), 'WONT_FIX');
+    assert.equal(translateQuickPickToIssueStatus(MUTE_TRANSITION_STATES[1]), 'FALSE_POSITIVE');
   });
 });
