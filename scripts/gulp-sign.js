c7320c6343936a7221db2fc7ff88afb04c0c9fed
Sebastien Vermeille <sebastien.vermeille@sonarsource.com>
2023-06-20 08:08:15 +0200

    BUILD-3065 Migrate sonarlint-vscode CI to Cirrus Adapt as the key is no longer provided in a file

--- scripts/gulp-sign.js
+++ scripts/gulp-sign.js
@@ -50,9 +50,9 @@ function getTransform(opts, keep) {
     };
 }
 
-async function sign(content, keyPath, passphrase) {
+async function sign(content, privateKeyArmored, passphrase) {
     const privateKey = await openpgp.decryptKey({
-        privateKey: await openpgp.readPrivateKey({ armoredKey: getKey(keyPath) }),
+        privateKey: await openpgp.readPrivateKey({ armoredKey: privateKeyArmored }),
         passphrase
     });
     const message = await openpgp.createMessage({ binary: content });
@@ -61,12 +61,4 @@ async function sign(content, keyPath, passphrase) {
         signingKeys: privateKey,
         detached: true
     });
-}
-
-function getKey(keyPath) {
-  try{
-    return fs.readFileSync(keyPath, 'utf8');
-  } catch (err) {
-    throw new Error(`Unable to get sign key ${keyPath}`);
-  }
 }
