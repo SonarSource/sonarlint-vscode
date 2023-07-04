2f62ebd3023e96d64cb838650851922cfb8aa358
Sebastien Vermeille <sebastien.vermeille@sonarsource.com>
2023-07-04 15:57:01 +0200

    BUILD-3065 Migrate sonarlint-vscode CI to Cirrus work_on_dogfood

--- scripts/dogfood.py
+++ scripts/dogfood.py
@@ -21,6 +21,8 @@ dogfoodJson = json.dumps({
     'url': f"$(ARTIFACTORY_URL)/sonarsource/org/sonarsource/sonarlint/vscode/sonarlint-vscode/{version}/sonarlint-vscode-{version}.vsix"
 })
 updateDogfoodJsonUrl = "$(ARTIFACTORY_URL)/sonarsource-public-builds/org/sonarsource/sonarlint/vscode/sonarlint-vscode/dogfood.json"
-response = requests.put(url=updateDogfoodJsonUrl, data=dogfoodJson, auth=('$(ARTIFACTORY_API_USER)', '$(ARTIFACTORY_API_KEY)'))
-if not response.status_code == 201:
-    sys.exit('[!] [{0}] Server Error'.format(response.status_code))
+
+#TODO: commented for safety
+# response = requests.put(url=updateDogfoodJsonUrl, data=dogfoodJson, auth=('$(ARTIFACTORY_API_USER)', '$(ARTIFACTORY_API_KEY)'))
+# if not response.status_code == 201:
+#     sys.exit('[!] [{0}] Server Error'.format(response.status_code))
