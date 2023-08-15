dd6828068dd8fc769922834529121550ad93a167
Jean-Baptiste Lievremont <jeanbaptiste.lievremont@sonarsource.com>
2023-08-15 17:50:55 +0200

    SLVSCODE-539 Show clean code attribute and impacts on rule description

--- src/commons.ts
+++ src/commons.ts
@@ -7,4 +7,5 @@ export namespace SonarLintDocumentation {
   export const CONNECTED_MODE = BASE_DOCS_URL + "team-features/connected-mode/";
   export const LANGUAGES_AND_RULES = BASE_DOCS_URL + "using-sonarlint/rules";
   export const COBOL_ANALYSIS = BASE_DOCS_URL + "using-sonarlint/rules/#cobol-analysis";
+  export const CLEAN_CODE_CONCEPTS = BASE_DOCS_URL + "concepts/clean-code/";
 }
