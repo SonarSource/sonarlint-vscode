239817ea915dd97c98b5764188d8d6caa03fb610
Jean-Baptiste Lievremont <jeanbaptiste.lievremont@sonarsource.com>
2023-10-23 14:54:42 +0200

    Fix SBOM generation

--- build-sonarlint/sbomGeneration.mjs
+++ build-sonarlint/sbomGeneration.mjs
@@ -10,13 +10,15 @@ import { exec } from 'child_process';
 import { info } from 'fancy-log';
 
 export default function cycloneDx() {
-  info('Starting task "cyclneDx"')
+  info('Starting task "cycloneDx"')
   const packageJSON = getPackageJSON();
   const version = packageJSON.version;
-  const cycloneDxCommand = `npm run cyclonedx-run -- -d --output sonarlint-vscode-${version}.sbom-cyclonedx.json`;
+  const cycloneDxCommand = `npm run cyclonedx-run -- --output-file sonarlint-vscode-${version}.sbom-cyclonedx.json`;
   exec(cycloneDxCommand, (err, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
+    if (err) {
+      throw new Error('Error running cyclonedx', err);
+    }
   });
 }
-
