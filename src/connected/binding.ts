790c4b971667f6b567212de1a144a9bab4781233
Sophio Japharidze <sophio.japharidze@sonarsource.com>
2024-01-30 14:10:58 +0100

    address review comment

--- src/connected/binding.ts
+++ src/connected/binding.ts
@@ -120,8 +120,8 @@ export class BindingService {
     const existingSettings = VSCode.workspace
       .getConfiguration(SONARLINT_CATEGORY, workspaceFolder)
       .get<ProjectBinding>(BINDING_SETTINGS);
-        if (existingSettings.projectKey === undefined) {
-            await VSCode.workspace
+    if (existingSettings.projectKey === undefined) {
+      await VSCode.workspace
         .getConfiguration(SONARLINT_CATEGORY, workspaceFolder)
         .update(BINDING_SETTINGS, { connectionId: params.connectionId, projectKey: params.projectKey });
     }
