76df086b0f49bb205b021e534fd174160c564339
Jean-Baptiste Lievremont <jeanbaptiste.lievremont@sonarsource.com>
2024-03-07 11:44:28 +0100

    Fix deadlock during initialization of backend, remove useless tests

--- test/globalsetup.ts
+++ test/globalsetup.ts
@@ -6,9 +6,15 @@
  * ------------------------------------------------------------------------------------------ */
 'use strict';
 
+import { logToSonarLintOutput } from '../src/util/logging';
 import { extension } from '../src/util/util';
 
-setup('ensure extension is ready', async function() {
+setup('ensure extension is ready', async function () {
+  logToSonarLintOutput(`>>>>>>>>>> Start  ${this.currentTest.fullTitle()}`);
   this.timeout(60_000);
   await extension.activate();
+});
+
+teardown(function() {
+  logToSonarLintOutput(`<<<<<<<<<< Finish ${this.currentTest.fullTitle()}`);
 });
