75b488ce11d7bfc25e520161fb457038cf68438a
Sophio Japharidze <sophio.japharidze@sonarsource.com>
2024-04-19 10:54:09 +0200

    Refactor FileSystemService using observer pattern

--- src/fileSystem/fileSystemSubscriber.ts
+++ src/fileSystem/fileSystemSubscriber.ts
@@ -7,3 +7,9 @@
 
 'use strict';
 
+import * as vscode from 'vscode';
+
+export interface FileSystemSubscriber {
+  onFile(folderUri: string, fileName: string, fullFileUri: vscode.Uri): void;
+  didRemoveWorkspaceFolder(workspaceFolderUri: vscode.Uri): void;
+}
