b980b2b1413428982fa3ecbba457aee1cceac35d
Sophio Japharidze <sophio.japharidze@sonarsource.com>
2024-07-25 13:34:56 +0200

    SLVSCODE-808 handle case when file content has been modified

--- src/fixSuggestions/fixSuggestionsService.ts
+++ src/fixSuggestions/fixSuggestionsService.ts
@@ -6,8 +6,9 @@ export async function showFixSuggestion(params : ShowFixSuggestionParams) {
 		const fileUri = vscode.Uri.parse(params.fileUri);
 		await vscode.window.showTextDocument(fileUri);
 		const wsedit = new vscode.WorkspaceEdit();
-		params.textEdits.forEach((edit) => {
+		params.textEdits.forEach(async (edit) => {
 			const range = new vscode.Range(edit.beforeLineRange.startLine, 0, edit.beforeLineRange.endLine, 10000);
+			isBeforeContentIdentical(fileUri, range, edit.before);
 			wsedit.replace(fileUri, range, edit.after, {label: 'preview', needsConfirmation: true});
 		});
         const result = await vscode.workspace.applyEdit(wsedit);
@@ -17,4 +18,12 @@ export async function showFixSuggestion(params : ShowFixSuggestionParams) {
     } catch (error) {
         console.error('Failed to apply edit:', error);
     }
+}
+
+async function isBeforeContentIdentical(fileUri: vscode.Uri, range: vscode.Range, before: string) {
+	const doc = await vscode.workspace.openTextDocument(fileUri);
+	const content = doc.getText(range);
+	if (content !== before) {
+		await vscode.window.showWarningMessage('The content of the file has changed. The fix suggestion may not be applicable.');
+	}
 }
