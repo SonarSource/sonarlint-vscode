a9ac485f8ce149649027184087a60d89bd644aa4
Sophio Japharidze <sophio.japharidze@sonarsource.com>
2025-01-02 11:01:27 +0100

    Update license headers for 2025

--- dogfood/src/test/suite/extension.test.ts
+++ dogfood/src/test/suite/extension.test.ts
@@ -1,6 +1,6 @@
 /* --------------------------------------------------------------------------------------------
  * SonarLint Dogfood
- * Copyright (C) 2021-2024 SonarSource SA
+ * Copyright (C) 2021-2025 SonarSource SA
  * sonarlint@sonarsource.com
  * Licensed under the LGPLv3 License. See LICENSE.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
--- its/src/test/helmSuite/helm.test.ts
+++ its/src/test/helmSuite/helm.test.ts
@@ -1,6 +1,6 @@
 /* --------------------------------------------------------------------------------------------
  * SonarLint for VisualStudio Code
- * Copyright (C) 2017-2024 SonarSource SA
+ * Copyright (C) 2017-2025 SonarSource SA
  * sonarlint@sonarsource.com
  * Licensed under the LGPLv3 License. See LICENSE.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
