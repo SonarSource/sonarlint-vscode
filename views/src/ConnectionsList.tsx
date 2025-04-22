f90d95626f5870d35ab197099ce21f257f9d5c75
Sophio Japharidze <sophio.japharidze@sonarsource.com>
2025-04-22 15:14:21 +0200

    SLVSCODE-1115 post message from webview to extension

--- views/src/ConnectionsList.tsx
+++ views/src/ConnectionsList.tsx
@@ -1,17 +1,31 @@
 import * as React from "react";
 
-interface Connection {
+interface ConnectionCheckResult {
+  connectionId: string;
+  success: boolean;
+  reason?: string;
+}
+
+interface ServerConnection {
   serverUrl: string;
   connectionId?: string;
 }
 
-export default () => {
-  const [connections, setConnections] = React.useState<Connection[]>([]);
+interface CloudConnection {
+  organizationKey: string;
+  connectionId?: string;
+}
+
+export default ({ vscode }) => {
+  const [serverConnections, setServerConnections] = React.useState<ServerConnection[]>([]);
+  const [cloudConnections, setCloudConnections] = React.useState<CloudConnection[]>([]);
   React.useEffect(() => {
     window.addEventListener('message', (event) => {
       const message = event.data;
-      if (message.command === 'setConnections') {
-        setConnections(message.connections);
+      if (message.command === 'setServerConnections') {
+        setServerConnections(message.connections);
+      } else if (message.command === 'setCloudConnections') {
+        setCloudConnections(message.connections);
       }
     });
     return () => { /* NOP */ };
@@ -19,14 +33,28 @@ export default () => {
 
   return (<>
     <h1>Welcome to the List of SonarQube Connections View!</h1>
+    <h2>Server Connections</h2>
     <ul title="Server Connections">
-      {connections.map((connection) => (
-        <li key={connection.serverUrl}>
+      {serverConnections.map((connection) => (
+        <li title={connection.serverUrl}>
           <span>{connection.serverUrl}</span>
           <br />
           <span>{connection.connectionId}</span>
         </li>
       ))}
     </ul>
+    <h2>Cloud Connections</h2>
+    <ul title = "Cloud Connections">
+      {cloudConnections.map((connection) => (
+        <li key={connection.organizationKey}>
+          <span>{connection.organizationKey}</span>
+          <br />
+          <span>{connection.connectionId}</span>
+        </li>
+      ))}
+    </ul>
+    <button onClick={() => { vscode.postMessage({ command: 'showNotification', displayMessage: 'This came from WebView' }) }}>
+      Show Demo Notification
+    </button>
   </>);
 };
