ba454d7002bf94d48a764f5c3e80c7b6569cc66f
Jean-Baptiste Lievremont <jeanbaptiste.lievremont@sonarsource.com>
2025-04-24 17:30:15 +0200

    SLVSCODE-1106 Switch to yarn, use Button from echoes-react

--- views/src/ConnectionsList.tsx
+++ views/src/ConnectionsList.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Button } from "@sonarsource/echoes-react";
 
 interface ConnectionCheckResult {
   connectionId: string;
@@ -9,11 +10,13 @@ interface ConnectionCheckResult {
 interface ServerConnection {
   serverUrl: string;
   connectionId?: string;
+  connectionCheckResult?: ConnectionCheckResult;
 }
 
 interface CloudConnection {
   organizationKey: string;
   connectionId?: string;
+  connectionCheckResult?: ConnectionCheckResult;
 }
 
 export default ({ vscode }) => {
@@ -21,6 +24,7 @@ export default ({ vscode }) => {
   const [cloudConnections, setCloudConnections] = React.useState<CloudConnection[]>([]);
   React.useEffect(() => {
     window.addEventListener('message', (event) => {
+      console.log(JSON.stringify(event));
       const message = event.data;
       if (message.command === 'setServerConnections') {
         setServerConnections(message.connections);
@@ -28,6 +32,7 @@ export default ({ vscode }) => {
         setCloudConnections(message.connections);
       }
     });
+    vscode.postMessage({ command: 'ready' });
     return () => { /* NOP */ };
   });
 
@@ -36,7 +41,7 @@ export default ({ vscode }) => {
     <h2>Server Connections</h2>
     <ul title="Server Connections">
       {serverConnections.map((connection) => (
-        <li title={connection.serverUrl}>
+        <li title={connection.serverUrl} key={connection.connectionId}>
           <span>{connection.serverUrl}</span>
           <br />
           <span>{connection.connectionId}</span>
@@ -46,15 +51,15 @@ export default ({ vscode }) => {
     <h2>Cloud Connections</h2>
     <ul title = "Cloud Connections">
       {cloudConnections.map((connection) => (
-        <li key={connection.organizationKey}>
+        <li title={connection.organizationKey} key={connection.connectionId}>
           <span>{connection.organizationKey}</span>
           <br />
           <span>{connection.connectionId}</span>
         </li>
       ))}
     </ul>
-    <button onClick={() => { vscode.postMessage({ command: 'showNotification', displayMessage: 'This came from WebView' }) }}>
+    <Button onClick={() => { vscode.postMessage({ command: 'showNotification', displayMessage: 'This came from WebView' }) }}>
       Show Demo Notification
-    </button>
+    </Button>
   </>);
 };
