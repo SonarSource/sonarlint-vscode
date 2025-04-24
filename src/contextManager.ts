9de9c07c5ed47bbcc041920d3efdac3fac665fe7
Sophio Japharidze <sophio.japharidze@sonarsource.com>
2025-04-24 11:51:02 +0200

    SLVSCODE-1122, SLVSCODE-1123 use more descriptive context key names

--- src/contextManager.ts
+++ src/contextManager.ts
@@ -10,8 +10,8 @@ import * as vscode from 'vscode';
 import { BindingService } from './connected/binding';
 import { allFalse, allTrue } from './rules/rules';
 
-const ONLY_CONNECTED_MODE_CONTEXT_KEY = 'sonarqube.onlyConnectedMode';
-const ONLY_STANDALONE_MODE_CONTEXT_KEY = 'sonarqube.onlyStandaloneMode';
+const SOME_CONNECTED_MODE_CONTEXT_KEY = 'sonarqube.someFoldersUseConnectedMode';
+const SOME_STANDALONE_MODE_CONTEXT_KEY = 'sonarqube.someFoldersUseStandaloneMode';
 const HAS_EXPLORED_ISSUE_LOCATIONS_CONTEXT_KEY = 'sonarqube.hasExploredIssueLocations';
 
 export class ContextManager {
@@ -29,16 +29,16 @@ export class ContextManager {
     const folderBindingStates = [...BindingService.instance.bindingStatePerFolder().values()];
     if (allTrue(folderBindingStates)) {
       // All folders are bound; Show hotspots view and hide rules view
-      vscode.commands.executeCommand('setContext', ONLY_CONNECTED_MODE_CONTEXT_KEY, true);
-      vscode.commands.executeCommand('setContext', ONLY_STANDALONE_MODE_CONTEXT_KEY, false);
+      vscode.commands.executeCommand('setContext', SOME_CONNECTED_MODE_CONTEXT_KEY, true);
+      vscode.commands.executeCommand('setContext', SOME_STANDALONE_MODE_CONTEXT_KEY, false);
     } else if (allFalse(folderBindingStates)) {
       // No folders are bound; Show rules view and hide hotspots view
-      vscode.commands.executeCommand('setContext', ONLY_CONNECTED_MODE_CONTEXT_KEY, false);
-      vscode.commands.executeCommand('setContext', ONLY_STANDALONE_MODE_CONTEXT_KEY, true);
+      vscode.commands.executeCommand('setContext', SOME_CONNECTED_MODE_CONTEXT_KEY, false);
+      vscode.commands.executeCommand('setContext', SOME_STANDALONE_MODE_CONTEXT_KEY, true);
     } else {
       // Some folders are bound and some are not; Show both views; Should be a corner case
-      vscode.commands.executeCommand('setContext', ONLY_CONNECTED_MODE_CONTEXT_KEY, true);
-      vscode.commands.executeCommand('setContext', ONLY_STANDALONE_MODE_CONTEXT_KEY, true);
+      vscode.commands.executeCommand('setContext', SOME_CONNECTED_MODE_CONTEXT_KEY, true);
+      vscode.commands.executeCommand('setContext', SOME_STANDALONE_MODE_CONTEXT_KEY, true);
     }
   }
 
@@ -47,8 +47,8 @@ export class ContextManager {
   }
 
   resetAllContexts() {
-    vscode.commands.executeCommand('setContext', ONLY_CONNECTED_MODE_CONTEXT_KEY, undefined);
-    vscode.commands.executeCommand('setContext', ONLY_STANDALONE_MODE_CONTEXT_KEY, undefined);
+    vscode.commands.executeCommand('setContext', SOME_CONNECTED_MODE_CONTEXT_KEY, undefined);
+    vscode.commands.executeCommand('setContext', SOME_STANDALONE_MODE_CONTEXT_KEY, undefined);
     vscode.commands.executeCommand('setContext', HAS_EXPLORED_ISSUE_LOCATIONS_CONTEXT_KEY, undefined);
   }
 
