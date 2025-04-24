a7866f943b99387bfd53d581376819edbca25514
Jean-Baptiste Lievremont <jeanbaptiste.lievremont@sonarsource.com>
2025-04-24 17:30:15 +0200

    SLVSCODE-1106 Switch to yarn, use Button from echoes-react

--- views/src/connectionsListIndex.tsx
+++ views/src/connectionsListIndex.tsx
@@ -1,7 +1,23 @@
 import * as React from "react";
+import { createIntl, createIntlCache, RawIntlProvider } from "react-intl";
 import { createRoot } from "react-dom/client";
+import { EchoesProvider } from "@sonarsource/echoes-react";
+
 import ConnectionsList from "./ConnectionsList";
+
+const cache = createIntlCache()
+
+const intl = createIntl({
+  locale: 'en-US',
+  messages: {}
+}, cache)
 
 const vscode = acquireVsCodeApi();
 
-createRoot(document.getElementById('root')!).render(<ConnectionsList vscode={vscode}/>)
+createRoot(document.getElementById('root')!).render(
+    <RawIntlProvider value={intl}>
+        <EchoesProvider>
+           <ConnectionsList vscode={vscode}/>
+        </EchoesProvider>
+    </RawIntlProvider>
+)
