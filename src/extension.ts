3b757173467122207f740fa05715af389d4393f3
Sophio Japharidze <sophio.japharidze@sonarsource.com>
2025-05-13 12:14:44 +0200

    SLVSCODE-1164 one-time migration of SonarQube Cloud tokens

--- src/extension.ts
+++ src/extension.ts
@@ -50,7 +50,7 @@ import { showRuleDescription } from './rules/rulepanel';
 import { AllRulesTreeDataProvider, LanguageNode, RuleNode, setRulesViewMessage, toggleRule } from './rules/rules';
 import { initScm, isIgnoredByScm } from './scm/scm';
 import { isFirstSecretDetected, showNotificationForFirstSecretsIssue } from './secrets/secrets';
-import { ConnectionSettingsService, migrateConnectedModeSettings } from './settings/connectionsettings';
+import { ConnectionSettingsService, migrateConnectedModeSettings, migrateSonarQubeCloudTokens } from './settings/connectionsettings';
 import {
   enableVerboseLogs,
   getCurrentConfiguration,
@@ -232,6 +232,8 @@ export async function activate(context: VSCode.ExtensionContext) {
   context.subscriptions.push(taintVulnerabilityCollection);
 
   ConnectionSettingsService.init(context, languageClient);
+  // SLVSCODE-1164; one time migration of SonarQube Cloud tokens to set region prefix
+  await migrateSonarQubeCloudTokens();
   NewCodeDefinitionService.init(context);
   FileSystemServiceImpl.init();
   SharedConnectedModeSettingsService.init(languageClient, FileSystemServiceImpl.instance, context);
