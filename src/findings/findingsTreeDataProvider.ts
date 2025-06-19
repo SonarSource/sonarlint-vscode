f1e93816d157868818d99953e06b037c55368365
Kirill Knize <kirill.knize@sonarsource.com>
2025-06-19 11:12:17 +0200

    SLVSCODE-1208 Adapt Language Model Tools to include taint vulnerabilities

--- src/findings/findingsTreeDataProvider.ts
+++ src/findings/findingsTreeDataProvider.ts
@@ -312,8 +312,11 @@ export class FindingsTreeDataProvider implements vscode.TreeDataProvider<Finding
     ));
   }
 
-  getHotspotsForFile(fileUri: string): FindingNode[] {
-    return this.findingsCache.get(fileUri)?.filter(finding => finding.findingType === FindingType.SecurityHotspot) || [];
+  getHotspotsAndTaintsForFile(fileUri: string): FindingNode[] {
+    return this.findingsCache.get(fileUri)
+      ?.filter(finding => finding.findingType === FindingType.SecurityHotspot
+        || finding.findingType === FindingType.TaintVulnerability)
+      || [];
   }
 
   hasFindings(): boolean {
