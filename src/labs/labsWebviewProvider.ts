1e54cebea3236dc08f042cdcbcbb61533296ad82
Sophio Japharidze <sophio.japharidze@sonarsource.com>
2025-11-04 14:43:05 +0100

    SLVSCODE-1407 add images and descriptions for labs features

--- src/labs/labsWebviewProvider.ts
+++ src/labs/labsWebviewProvider.ts
@@ -12,33 +12,87 @@ import * as vscode from 'vscode';
 import * as util from '../util/util';
 import { ResourceResolver } from '../util/webview';
 import { LABS_FEATURES } from './labsFeatures';
+import { SonarLintExtendedLanguageClient } from '../lsp/client';
+import { Commands } from '../util/commands';
+import { IdeLabsFlagManagementService } from './ideLabsFlagManagementService';
 
 export class LabsWebviewProvider implements vscode.WebviewViewProvider {
   private _view?: vscode.WebviewView;
 
-  constructor(private readonly _extensionContext: vscode.ExtensionContext) {}
+  constructor(
+    private readonly extensionContext: vscode.ExtensionContext,
+    private readonly languageClient: SonarLintExtendedLanguageClient
+  ) {}
 
   public resolveWebviewView(
     webviewView: vscode.WebviewView,
-    context: vscode.WebviewViewResolveContext,
+    _context: vscode.WebviewViewResolveContext,
     _token: vscode.CancellationToken
   ) {
     this._view = webviewView;
 
     webviewView.webview.options = {
       enableScripts: true,
-      localResourceRoots: [this._extensionContext.extensionUri]
+      localResourceRoots: [this.extensionContext.extensionUri]
     };
 
     webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+
+    // Handle messages from the webview
+    webviewView.webview.onDidReceiveMessage(
+      message => {
+        switch (message.command) {
+          case 'signup':
+            this.handleSignup(message.email);
+            break;
+          case 'openLink':
+            this.handleOpenLink(message.linkId);
+            break;
+        }
+      },
+      undefined,
+      this.extensionContext.subscriptions
+    );
+  }
+
+  private async handleSignup(email: string) {
+    try {
+      this._view?.webview.postMessage({ command: 'signupLoading' });
+      const response = await this.languageClient.joinIdeLabsProgram(email, vscode.env.appName);
+      if (response.success) {
+        await IdeLabsFlagManagementService.instance.enableIdeLabs();
+        
+        vscode.window.showInformationMessage('Congratulations! You have joined SonarQube for IDE Labs!');
+        this._view?.webview.postMessage({ command: 'signupSuccess' });
+      } else {
+        this._view?.webview.postMessage({
+          command: 'signupError',
+          message: response.message || 'Failed to join SonarQube for IDE Labs.'
+        });
+      }
+    } catch (error) {
+      this._view?.webview.postMessage({
+        command: 'signupError',
+        message: `Failed to join Labs program: ${error.message}`
+      });
+    }
+  }
+
+  private handleOpenLink(linkId: string) {
+    const utmContent = 'ide-labs-signup';
+    vscode.commands.executeCommand(Commands.TRIGGER_HELP_AND_FEEDBACK_LINK, {
+      id: linkId,
+      utm: { content: utmContent, term: linkId }
+    });
   }
 
   private _getHtmlForWebview(webview: vscode.Webview): string {
+    IdeLabsFlagManagementService.instance.disableIdeLabs();
     // Load HTML template
     const templatePath = util.resolveExtensionFile('webview-ui', 'labs.html');
     const template = fs.readFileSync(templatePath.fsPath, 'utf-8');
 
-    const resolver = new ResourceResolver(this._extensionContext, webview);
+    const resolver = new ResourceResolver(this.extensionContext, webview);
 
     // Resolve resource paths
     const styleSrc = resolver.resolve('styles', 'labs.css');
@@ -50,12 +104,16 @@ export class LabsWebviewProvider implements vscode.WebviewViewProvider {
       imageUrl: resolver.resolve('images', feature.imageFile)
     }));
 
+    // Check if user has already signed up
+    const isSignedUp = IdeLabsFlagManagementService.instance.isIdeLabsEnabled();
+
     // Replace placeholders in template
     return template
       .replaceAll('{{cspSource}}', webview.cspSource)
       .replace('{{styleSrc}}', styleSrc)
       .replace('{{scriptSrc}}', scriptSrc)
       .replace('{{featuresJson}}', JSON.stringify(featuresWithImages, null, 2))
-      .replace('{{featureCount}}', LABS_FEATURES.length.toString());
+      .replace('{{featureCount}}', LABS_FEATURES.length.toString())
+      .replace('{{isSignedUp}}', isSignedUp.toString());
   }
 }
