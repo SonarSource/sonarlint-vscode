e18b7603a53fd66e04f766dd779b5fdf938091ee
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2025-11-13 19:17:00 +0100

    SLVSCODE-1431 Update code file license headers to "SonarSource Sàrl"

--- src/monitoring/monitoring.ts
+++ src/monitoring/monitoring.ts
@@ -1,6 +1,6 @@
 /* --------------------------------------------------------------------------------------------
  * SonarLint for VisualStudio Code
- * Copyright (C) 2017-2024 SonarSource SA
+ * Copyright (C) 2017-2024 SonarSource Sàrl
  * sonarlint@sonarsource.com
  * Licensed under the LGPLv3 License. See LICENSE.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
