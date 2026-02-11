d535188611ee1db4c148d489fe02231e6c576780
Vasileios Naskos <vasileios.naskos@sonarsource.com>
2026-02-11 11:54:23 +0100

    Create remediation panel that shows Open In IDE events

---
+++ src/remediationPanel/remediationService.ts
@@ -0,0 +1,129 @@
+/* --------------------------------------------------------------------------------------------
+ * SonarLint for VisualStudio Code
+ * Copyright (C) 2017-2025 SonarSource SA
+ * sonarlint@sonarsource.com
+ * Licensed under the LGPLv3 License. See LICENSE.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+'use strict';
+
+import * as vscode from 'vscode';
+import { ExtendedClient } from '../lsp/protocol';
+import {
+  RemediationEvent,
+  RemediationEventType,
+  IssueRemediationEvent,
+  HotspotRemediationEvent
+} from './remediationEvent';
+
+const BATCH_THRESHOLD_MS = 5000;
+
+export class RemediationService {
+  private static _instance: RemediationService;
+  private events: RemediationEvent[] = [];
+  private lastEventTimestamp: number = 0;
+  private eventChangeEmitter = new vscode.EventEmitter<RemediationEvent[]>();
+  public readonly onEventsChanged = this.eventChangeEmitter.event;
+
+  private constructor() {}
+
+  static init(): void {
+    if (!RemediationService._instance) {
+      RemediationService._instance = new RemediationService();
+    }
+  }
+
+  static get instance(): RemediationService {
+    if (!RemediationService._instance) {
+      throw new Error('RemediationService not initialized. Call RemediationService.init() first.');
+    }
+    return RemediationService._instance;
+  }
+
+  trackIssueEvent(issue: ExtendedClient.Issue): void {
+    this.checkAndClearBatch();
+
+    const fileUri = issue.fileUri;
+    const filePath = this.getRelativePath(fileUri);
+    const textRange = issue.textRange;
+
+    const event: IssueRemediationEvent = {
+      id: this.generateId(),
+      type: RemediationEventType.OPEN_ISSUE,
+      timestamp: Date.now(),
+      fileUri,
+      filePath,
+      message: issue.message,
+      ruleKey: issue.ruleKey,
+      issue,
+      textRange
+    };
+
+    this.addEvent(event);
+  }
+
+  trackHotspotEvent(hotspot: ExtendedClient.RemoteHotspot): void {
+    this.checkAndClearBatch();
+
+    const fileUri = `file://${hotspot.ideFilePath}`;
+    const filePath = this.getRelativePath(fileUri);
+    const textRange = hotspot.textRange;
+
+    const event: HotspotRemediationEvent = {
+      id: this.generateId(),
+      type: RemediationEventType.OPEN_HOTSPOT,
+      timestamp: Date.now(),
+      fileUri,
+      filePath,
+      message: hotspot.message,
+      ruleKey: hotspot.rule?.key,
+      hotspot,
+      textRange
+    };
+
+    this.addEvent(event);
+  }
+
+  private generateId(): string {
+    return `${Date.now()}-${Math.random().toString(36).substr(2, 9)}`;
+  }
+
+  clearEvents(): void {
+    this.events = [];
+    this.lastEventTimestamp = 0;
+    this.eventChangeEmitter.fire(this.events);
+  }
+
+  getEvents(): ReadonlyArray<RemediationEvent> {
+    return this.events;
+  }
+
+  private checkAndClearBatch(): void {
+    const now = Date.now();
+    if (this.events.length > 0 && now - this.lastEventTimestamp > BATCH_THRESHOLD_MS) {
+      this.events = [];
+    }
+  }
+
+  private addEvent(event: RemediationEvent): void {
+    this.events.push(event);
+    this.lastEventTimestamp = event.timestamp;
+    this.eventChangeEmitter.fire(this.events);
+  }
+
+  private getRelativePath(fileUri: string): string {
+    try {
+      const uri = vscode.Uri.parse(fileUri);
+      const workspaceFolder = vscode.workspace.getWorkspaceFolder(uri);
+      if (workspaceFolder) {
+        return vscode.workspace.asRelativePath(uri, false);
+      }
+      return uri.fsPath;
+    } catch (error) {
+      return fileUri;
+    }
+  }
+
+  dispose(): void {
+    this.eventChangeEmitter.dispose();
+  }
+}
---
+++ src/remediationPanel/remediationWebviewProvider.ts
@@ -0,0 +1,145 @@
+/* --------------------------------------------------------------------------------------------
+ * SonarLint for VisualStudio Code
+ * Copyright (C) 2017-2025 SonarSource SA
+ * sonarlint@sonarsource.com
+ * Licensed under the LGPLv3 License. See LICENSE.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+'use strict';
+
+import * as vscode from 'vscode';
+import { RemediationService } from './remediationService';
+import { generateRemediationPanelContent } from './remediationPanelContent';
+import { ResourceResolver } from '../util/webview';
+import { RemediationEventType } from './remediationEvent';
+import { IssueService } from '../issue/issue';
+import { Commands } from '../util/commands';
+
+export class RemediationWebviewProvider implements vscode.WebviewViewProvider {
+  private _view?: vscode.WebviewView;
+  private disposables: vscode.Disposable[] = [];
+
+  constructor(private readonly context: vscode.ExtensionContext) {
+    RemediationService.instance.onEventsChanged(() => {
+      this.updateContent();
+      this.expandView();
+    });
+  }
+
+  resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    context: vscode.WebviewViewResolveContext,
+    token: vscode.CancellationToken
+  ): void | Thenable<void> {
+    this._view = webviewView;
+
+    webviewView.webview.options = {
+      enableScripts: true,
+      localResourceRoots: [
+        vscode.Uri.joinPath(this.context.extensionUri, 'styles'),
+        vscode.Uri.joinPath(this.context.extensionUri, 'images')
+      ]
+    };
+
+    this.updateContent();
+
+    webviewView.webview.onDidReceiveMessage(
+      message => {
+        switch (message.command) {
+          case 'navigateToEvent':
+            this.handleNavigateToEvent(message.eventId);
+            break;
+        }
+      },
+      undefined,
+      this.disposables
+    );
+  }
+
+  private updateContent(): void {
+    if (!this._view) {
+      return;
+    }
+
+    const events = RemediationService.instance.getEvents();
+    const resolver = new ResourceResolver(this.context, this._view.webview);
+    const nonce = this.getNonce();
+    this._view.webview.html = generateRemediationPanelContent(
+      events,
+      resolver,
+      this._view.webview.cspSource,
+      nonce
+    );
+  }
+
+  private expandView(): void {
+    if (this._view && RemediationService.instance.getEvents().length > 0) {
+      this._view.show?.(true);
+    }
+  }
+
+  private getNonce(): string {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < 32; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  }
+
+  private async handleNavigateToEvent(eventId: string): Promise<void> {
+    const events = RemediationService.instance.getEvents();
+    const event = events.find(e => e.id === eventId);
+
+    if (!event) {
+      return;
+    }
+
+    try {
+      switch (event.type) {
+        case RemediationEventType.OPEN_ISSUE:
+          await IssueService.showIssue(event.issue);
+          break;
+        case RemediationEventType.OPEN_HOTSPOT:
+          await this.openHotspotFile(event.hotspot);
+          break;
+        case RemediationEventType.VIEW_FIX_SUGGESTION:
+          // Future implementation
+          break;
+      }
+    } catch (error) {
+      vscode.window.showErrorMessage(`Failed to navigate to event: ${error.message}`);
+    }
+  }
+
+  private async openHotspotFile(hotspot: any): Promise<void> {
+    try {
+      const foundUris = await vscode.workspace.findFiles(`**/${hotspot.ideFilePath}`);
+      if (foundUris.length === 0) {
+        vscode.window.showErrorMessage(`Could not find file: ${hotspot.ideFilePath}`);
+        return;
+      }
+
+      const documentUri = foundUris[0];
+      const editor = await vscode.window.showTextDocument(documentUri);
+
+      if (hotspot.textRange) {
+        const range = new vscode.Range(
+          hotspot.textRange.startLine - 1,
+          hotspot.textRange.startLineOffset,
+          hotspot.textRange.endLine - 1,
+          hotspot.textRange.endLineOffset
+        );
+        editor.selection = new vscode.Selection(range.start, range.end);
+        editor.revealRange(range, vscode.TextEditorRevealType.InCenter);
+      }
+
+      await vscode.commands.executeCommand(Commands.SHOW_HOTSPOT_DESCRIPTION);
+    } catch (error) {
+      vscode.window.showErrorMessage(`Failed to open hotspot: ${error.message}`);
+    }
+  }
+
+  dispose(): void {
+    this.disposables.forEach(d => d.dispose());
+  }
+}
