8f7a6b0af562ca020cda755036cfa91b5ec69f80
Viacheslav Tsuvariev <viacheslav.tsuvariev@sonarsource.com>
2026-04-01 16:55:05 +0200

    DEX-26 Update license headers

--- README.md
+++ README.md
@@ -44,7 +44,7 @@ Issue tracker (read-only): https://jira.sonarsource.com/browse/SLVSCODE
 
 ## License
 
-Copyright 2017-2025 SonarSource.
+Copyright SonarSource.
 
 Licensed under the [GNU Lesser General Public License, Version 3.0](http://www.gnu.org/licenses/lgpl.txt)
 
--- webview-ui/labs.js
+++ webview-ui/labs.js
@@ -1,6 +1,6 @@
 /* --------------------------------------------------------------------------------------------
  * SonarLint for VisualStudio Code
- * Copyright (C) 2017-2025 SonarSource SA
+ * Copyright (C) SonarSource Sàrl
  * sonarlint@sonarsource.com
  * Licensed under the LGPLv3 License. See LICENSE.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
