f2755a7404997d7a0acb830d566f1ecd041de30e
Damien Urruty <damien.urruty@sonarsource.com>
2026-09-18 15:12:55 +0200

    SLVSCODE-1923 Update dependency mocha to v12

--- dogfood/src/test/suite/index.ts
+++ dogfood/src/test/suite/index.ts
@@ -1,9 +1,10 @@
 import { glob } from 'glob';
-import * as Mocha from 'mocha';
+import * as mochaNs from 'mocha';
 import * as path from 'path';
 
+const Mocha = ('default' in mochaNs ? mochaNs.default : mochaNs) as typeof mochaNs;
+
 export function run(): Promise<void> {
-  // Create the mocha test
   const mocha = new Mocha({
     ui: 'tdd',
     color: true
@@ -12,12 +13,10 @@ export function run(): Promise<void> {
   const testsRoot = path.resolve(__dirname, '..');
 
   return new Promise((c, e) => {
-    glob('**/**.test.js', { cwd: testsRoot }).then((files) => {
-      // Add files to the test suite
+    glob('**/**.test.js', { cwd: testsRoot }).then(files => {
       files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
       try {
-        // Run the mocha test
         mocha.run(failures => {
           if (failures > 0) {
             e(new Error(`${failures} tests failed.`));
---
+++ its/src/test/runMochaSuite.ts
@@ -0,0 +1,38 @@
+/* --------------------------------------------------------------------------------------------
+ * SonarLint for VisualStudio Code
+ * Copyright (C) SonarSource Sàrl
+ * sonarlint@sonarsource.com
+ * Licensed under the LGPLv3 License. See LICENSE.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+import { glob } from 'glob';
+import * as mochaNs from 'mocha';
+import * as path from 'node:path';
+
+const Mocha = ('default' in mochaNs ? mochaNs.default : mochaNs) as typeof mochaNs;
+
+export function runMochaSuite(xmlFileName: string) {
+  return (testsRoot: string, cb: (error: any, failures?: number) => void): void => {
+    const mocha = new Mocha({
+      ui: 'tdd',
+      reporter: 'mocha-multi-reporters',
+      reporterOptions: {
+        reporterEnabled: 'spec, xunit',
+        xunitReporterOptions: {
+          output: path.resolve(__dirname, '..', xmlFileName)
+        }
+      },
+      color: true
+    });
+
+    glob('**/**.test.js', { cwd: testsRoot })
+      .then(files => {
+        files.forEach(file => mocha.addFile(path.resolve(testsRoot, file)));
+        try {
+          mocha.run(failures => cb(null, failures));
+        } catch (err) {
+          cb(err);
+        }
+      })
+      .catch((err: Error) => cb(err));
+  };
+}
